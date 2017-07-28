732352bec27f693890754a1f8a5d469370975ff8
zhaoji <zhaoji1012@gmail.com>
2017-07-28 23:15:27 +0800

    add jackson-modlue-scala

---
+++ src/main/scala/x/h/p/m/BeanConfig.java
@@ -0,0 +1,23 @@
+package x.h.p.m;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.scala.DefaultScalaModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhangyi on 7/20/16.
+ */
+@Configuration
+public class BeanConfig {
+    @Bean(name = "JsonMapper")
+    public ObjectMapper jsonMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.registerModule(new DefaultScalaModule());
+
+        return objectMapper;
+    }
+}
--- src/main/scala/x/h/p/m/HelloWorldController.scala
+++ src/main/scala/x/h/p/m/HelloWorldController.scala
@@ -9,7 +9,25 @@ import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
 @Controller
 @RequestMapping(value = Array("/"))
 class HelloWorldController {
+
+
   @RequestMapping(value = Array("/helloWorld"))
   @ResponseBody
   def home= "Hello World"
+
+
+  case class  Person(age:Int,any: Any)
+  class anotherPerson(age:Int)
+
+  @RequestMapping(value = Array("/map"))
+  @ResponseBody
+  def map= Map("key"->"value")
+
+  @RequestMapping(value = Array("/person"))
+  @ResponseBody
+  def thePerson= new Person(12,Map("key"->"value","ddd"->List("de")))
+
+  @RequestMapping(value = Array("/get406"))
+  @ResponseBody
+  def get406= new  anotherPerson(12)
 }
